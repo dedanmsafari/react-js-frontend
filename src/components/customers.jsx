131c6c8d066697e7dd363995c4c3f01e4c0207ef
dedanmsafari <dedan.msafari@gmail.com>
2019-08-07 10:59:07 +0300

    removes unused components

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React  from 'react'
 const Customers = () => {
     return <h1>Customers</h1>;
 }
--- src/components/rentals.jsx
+++ src/components/rentals.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React  from 'react'
 
 const Rentals = () => {
     return <h1>Rental found</h1>;
