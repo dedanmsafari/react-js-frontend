4265839307d3d7147f9cb6bb3c96f290d223d83c
dedanmsafari <dedan.msafari@gmail.com>
2019-08-21 12:06:19 +0300

    renamed file names

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navbar';
 import MovieForm from './components/movieForm';
+import loginForm from './components/loginForm';
 import './App.css';
 function App() {
   return (
@@ -14,12 +15,13 @@ function App() {
 <main className ="Container">
 {/* < Movies /> */}
 <Switch>
-<Route path="/movies/:id" component={MovieForm}></Route>
-<Route path="/movies" component={Movies}></Route>
-<Route path="/customers" component={Customers}></Route>
-<Route path="/rentals" component={Rentals}></Route>
-<Route path="/not-found" component={NotFound}></Route>
-<Redirect from="/" exact to="/movies"/>
+<Route path="/login" component={loginForm}/>
+<Route path="/movies/:id" component={MovieForm}/>
+<Route path="/movies" component={Movies}/>
+<Route path="/customers" component={Customers}/>
+<Route path="/rentals" component={Rentals}/>
+<Route path="/not-found" component={NotFound}/>
+ <Redirect from="/" exact to="/movies"/>
 <Redirect to="/not-found"/>
 </Switch>
 </main>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
