5fa2c1fb49308f72a530fc60f91967f607a6f9d9
dedanmsafari <dedan.msafari@gmail.com>
2019-09-09 22:06:43 +0300

    creating a movieform

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,16 +1,52 @@
 import React  from "react";
-const MovieForm = ({ match, history }) => {
-  return (
-    <div>
-      <h1>{`MovieForm = ${match.params.id}`}</h1>
-      <button
-        onClick={() => history.push("/movies")}
-        className="badge badge-info"
-      >
-        Save
-      </button>
-    </div>
-  );
+import Joi from "joi-browser";
+import Form from "./common/form";
+import { getMovies, saveMovie } from '../services/fakeMovieService';
+import { getGenres } from "../services/fakeGenreService";
+
+class MovieForm extends Form {
+state = {
+data: {
+title:"",
+genreId:"",
+numberInStock:"",
+dailyRentalRate:""
+},
+genres:[],
+errors:{}
 };
+
+schema ={
+
+_id: Joi.string(),
+title: Joi.string().required().label("Title"),
+genreId: Joi.string().required().label("Genre"),
+numberInStock: Joi.number().required().min(0).max(100).label("Number in Stock"),
+dailyRentalRate: Joi.number().required().min(0).max(10).label("Daily Rental Rate")
+};
+componentDidMount(){
+const genres = getGenres();
+this.setState({genres});
+
+const movieId = this.props.match.params.id;
+if (movieId === "new") return;
+
+const movie = getMovie(movieId);
+
+if (!movie) return this.props.history.replace("/not-found");
+
+this.setState({ data: this.mapToViewModel(movie)});
+}
+
+mapToViewModel(movie){
+return{
+_id: movie._id,
+title: movie.title,
+genreId: movie.genre._id,
+numberInStock: movie.numberInStock,
+dailyRentalRate: movie.dailyRentalRate
+};
+}
+
 
 export default MovieForm;
